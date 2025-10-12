cecf4ec07a5c448a44c46cd37e2421f4d78976a5
Mihaela Catan <mihaelacatan7@gmail.com>
2025-10-12 22:23:31 +0300

    correct client_main to get folder as parameter

--- LAB1/client_main.py
+++ LAB1/client_main.py
@@ -1,19 +1,17 @@
 import sys
-from start_server import start_server
 from download_file import download_file
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python client_main.py <host> <port> <filename>")
+    if len(sys.argv) != 5:
+        print("Usage: python client_main.py <host> <port> <url_path> <save_dir>")
         sys.exit(1)
-    
+
     host = sys.argv[1]
     port = int(sys.argv[2])
-    filename = sys.argv[3]
-    
-    save_dir = './downloads'
-    
-    download_file(host, port, filename, save_dir)
+    url_path = sys.argv[3]
+    save_dir = sys.argv[4]
+
+    download_file(host, port, url_path, save_dir)
 
 if __name__ == '__main__':
     main()
